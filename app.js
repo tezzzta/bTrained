8fc33919b3099c6632eb1c8ce947f05d4bfdb232
tomas223-gr <tomasrg22344@gmail.com>
2024-11-26 22:25:27 -0500

    Update app.js
    Co-Authored-By: Alejandro Testa <187576994+tezzzta@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,11 @@
+// Declarar dos variables con números
+let numero1 = 5;
+let numero2 = 10;
+
+// Sumar los números
+let suma = numero1 + numero2;
+
+// Mostrar el resultado en la consola
+console.log("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+
+// Mostrar un mensaje en una ventana emerg
