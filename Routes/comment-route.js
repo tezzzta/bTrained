ebf93feda26299225d3a9706a31601d278c24fec
Alejandro Testa <a.s.testa.c@gmail.com>
2025-02-23 17:14:36 -0500

    commit-2302
    Autenticación, end of the session

--- Routes/comment-route.js
+++ Routes/comment-route.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/authmiddl'); // Asegúrate de importar correctamente el middleware
 const CommentController = require('../controllers/comment-controller');
 
+
 // Ruta para crear un comentario
-router.post('/crear', CommentController.createComment);
+router.post('/crear', authenticate, CommentController.createComment);
 
 // Ruta para obtener los comentarios de una transición
 router.get('/comentarios/:transitionId', CommentController.getCommentsByTransition);
 
 // Ruta para eliminar un comentario
-router.delete('/:commentId', CommentController.deleteComment);
+router.delete('/:commentId', authenticate, CommentController.deleteComment);
 
 module.exports = router;
--- Routes/transition-route.js
+++ Routes/transition-route.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/authmiddl'); // Asegúrate de importar correctamente el middleware
 const TransitionController = require('../controllers/transition-controller');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }); // Configura multer para guardar archivos en la carpeta 'uploads'
 
-// Ruta para crear una nueva transición
-router.post('/create', TransitionController.createTransition);
+// Ruta para crear una nueva transición con imagen
+router.post('/create', authenticate, upload.single('image'), TransitionController.createTransition);
 
 // Ruta para obtener todas las transiciones
-router.get('/', TransitionController.getAllTransitions);
+router.get('/', authenticate, TransitionController.getAllTransitions);
 
 // Ruta para obtener una transición específica
 router.get('/:id', TransitionController.getTransitionById);
 
 // Ruta para editar una transición
-router.put('/:id', TransitionController.updateTransition);
+router.put('/:id', authenticate, TransitionController.updateTransition);
 
 // Ruta para eliminar una transición
-router.delete('/:id', TransitionController.deleteTransitionById);
+router.delete('/:id', authenticate, TransitionController.deleteTransitionById);
 
 module.exports = router;
