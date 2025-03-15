ff25d682c66ee625360c5bd512628c7c20a77bb9
Alejandro Testa <a.s.testa.c@gmail.com>
2025-03-15 01:15:54 -0500

    React-Bootstrap
    reactt

---
+++ frontend/tailwindcss4/src/Firstn.tsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+
+
+function Botton() {
+    return (
+        <> 
+          
+            <Button variant="danger">Danger</Button>
+
+        </>
+
+    );
+}
+
+export default Botton;
