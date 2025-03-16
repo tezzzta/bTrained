18aa6e61f45eb04c214928be5ef69f62cae290f5
Alejandro Testa <a.s.testa.c@gmail.com>
2025-03-16 02:41:36 -0500

    React-Arreglado
    Se importa bootstrap para manejar componentes

--- frontend/tailwindcss4/src/Firstn.tsx
+++ frontend/tailwindcss4/src/Firstn.tsx
@@ -1,15 +1,13 @@
+import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 
-
-function Botton() {
-    return (
-        <> 
-          
-            <Button variant="danger">Danger</Button>
-
-        </>
-
-    );
+function ButtonExample() {
+  return (
+    <Button variant="primary">
+      Profile <Badge bg="secondary">9</Badge>
+      <span className="visually-hidden"> unread messages</span>
+    </Button>
+  );
 }
 
-export default Botton;
+export default ButtonExample;
---
+++ frontend/tailwindcss4/src/Headerr.tsx
@@ -0,0 +1,35 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap
+
+function Header() {
+  return (
+    <header className="py-3 mb-4 border-bottom">
+      <Container className="d-flex flex-wrap justify-content-center">
+        <Navbar.Brand href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <span className="fs-4">Galería</span>
+        </Navbar.Brand>
+        <Nav className="nav-pills">
+          <Nav.Item>
+            <Nav.Link href="/" className="active" style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 'bold' }}>
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/crear" style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 'bold' }}>
+              Crear Transición
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/repertorio" style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 'bold' }}>
+              Repertorio
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
