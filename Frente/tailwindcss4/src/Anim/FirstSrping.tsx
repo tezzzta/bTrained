b3b25e39950351a470a9cccacf57413835990315
Alejandro Testa <a.s.testa.c@gmail.com>
2025-04-10 04:07:19 -0500

    10-04*-2025

--- Frente/tailwindcss4/src/Anim/FirstSrping.tsx
+++ Frente/tailwindcss4/src/Anim/FirstSrping.tsx
@@ -1,76 +1,94 @@
 import {  useSpringRef,useSpring, animated } from '@react-spring/web'
 import {motion} from 'framer-motion'
 
-const Apicomponent = () =>{
-  const api = useSpringRef<{x: number}>()
-  const springs = useSpring({
-    ref: api,
-    from: { x: 0 },
-  })
-  const handleClick = () => {
-    api.start({
-      to: {
-        x: springs.x.get() === 100 ? 0 : 100,
-      },
-    })
-  }
-
-  return (
-    <div className="flex-container"> 
 
 
-        <div>
-        <animated.div
-        onClick={handleClick}
+// ESTE ES UNA INTERFAZ PARA RECIBIR INFO DE DATA/PADRE
+interface OtroComponenteProps {
+  formData: {
+    nombre: string;
+    template: string;
+  };
+}
+
+
+// const Apicomponent = () =>{
+//   const api = useSpringRef<{x: number}>()
+//   const springs = useSpring({
+//     ref: api,
+//     from: { x: 0 },
+//   })
+//   const handleClick = () => {
+//     api.start({
+//       to: {
+//         x: springs.x.get() === 100 ? 0 : 100,
+//       },
+//     })
+//   }
+
+//   return (
+//     <div className="flex-container"> 
+
+
+//         <div>
+//         <animated.div
+//         onClick={handleClick}
+//         style={{
+//           width: 80,
+//           height: 80,
+//           background: '#ff6d6d',
+//           borderRadius: 8,
+//           transform: springs.x.to((x) => `translateX(${x}px)`),
+//         }as any  }
+//       />
+//         </div>
+//       <span>Render ID – {Math.random()}</span>
+//     </div>
+//   )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function MyComponent({formData}:OtroComponenteProps ){
+      const springs = useSpring({
+          from: { x: 0 },
+          to: { x: 100 },
+        })
+      return (
+        <motion.div
+          initial={{ opacity: 0, y: 0}} 
+          animate={{ opacity: 1, y: ["7px", "-15px", "-1px"] }} 
+          transition={{ duration: 1, delay: 2 }} 
+        
+        >
+          <animated.div
         style={{
+          transform: springs.x.to((x) => `translateX(${x}px)`), 
           width: 80,
           height: 80,
           background: '#ff6d6d',
           borderRadius: 8,
-          transform: springs.x.to((x) => `translateX(${x}px)`),
-        }as any  }
-      />
+        }}
+      
+        /> 
+
+        <div>
+        <p>Nombre: {formData.nombre}</p>
+        <p>Template: {formData.template}</p>
         </div>
-      <span>Render ID – {Math.random()}</span>
-    </div>
-  )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default function MyComponent() {
-    const springs = useSpring({
-        from: { x: 0 },
-        to: { x: 100 },
-      })
-    return (
-      <motion.div
-        initial={{ opacity: 0, y: 0}} 
-        animate={{ opacity: 1, y: ["7px", "-15px", "-1px"] }} 
-        transition={{ duration: 1, delay: 2 }} 
-      
-      >
-        <animated.div
-      style={{
-        transform: springs.x.to((x) => `translateX(${x}px)`), 
-        width: 80,
-        height: 80,
-        background: '#ff6d6d',
-        borderRadius: 8,
-      }}
-      /> 
-      </motion.div>
-      
-    )
-  }
+          
+        </motion.div>
+        
+      )
+    }
   
