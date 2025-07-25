872285b1bd3dd906bf2c46b9dafe7f2488626948
Alejandro Testa <a.s.testa.c@gmail.com>
2025-07-24 23:14:38 -0500

    [FIX] Updating to Deploy

---
+++ frontend/tailwindcss4/src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react';
+// import './App.css';
+// import Botone from './Firstn';
+import Header from './Headerr';
+import { WelcomeSection, Gallery, CarouselComponent} from './components/Aplication';
+
+function App() {
+
+  return (
+    <div className="App w-100">
+        <Header />
+        <WelcomeSection />
+        <Gallery />
+        <CarouselComponent /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwindcss4/src/components/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './harry.css';
+
+// Componente de Bienvenida
+const WelcomeSection: React.FC = () => {
+  return (
+    <Container className="py-5 text-center text-white welcome-container">
+      <h1>Bienvenido</h1>
+      <p>Para estudiar, para distraerte o para crear...</p>
+    </Container>
+  );
+};
+
+export default WelcomeSection;
